d9976252549bed2cff23e302cafb47f7455e21b4
jrogosky <jrogosky@gmail.com>
2018-01-01 12:21:49 -0500

    Added comments and average functions

--- TempScroll.py
+++ TempScroll.py
@@ -1,20 +1,21 @@
 from microbit import *
 
+#Scrolls the current temperature on the dipslay
 def ScrollCurrentTemp():
-    curr = temperature()
+    curr = temperature() - 3
     curr = round((curr * 9) / 5 + 32)
-    display.scroll(str(curr) + "°F")
+    display.scroll(str(curr) + "F")
 
+#Scrolls the average temperature on the display.  Uses getAvg
 def ScrollAvgTemp():
-    total = 0
-    for i in temps:
-        total += i
-    avg = round((((total / len(temps)) * 9) / 5) + 32)
-    display.scroll(str(avg) + "°F")
+    avg = getAvg()   
+    display.scroll(str(avg) + "F")
     
+#Displays a symbol corresponding to the temperature    
 def weather(temp):
     temp = round((temp * 9) / 5 + 32)
     if temp < 30:
+        #needs replaced. this is a "tree"
         display.show(Image.XMAS)
     if temp >= 30 and temp < 50:
         display.show(Image.MEH)
@@ -23,10 +24,21 @@ def weather(temp):
     if temp >= 80:
         display.show(Image.SAD)
         
-        
+#calculates the average temperature over a minute
+def getAvg():
+    total = 0
+    for i in temps:
+        total += i
+    avg = round((((total / len(temps)) * 9) / 5) + 32)
+    return avg
+     
+#initialize the main list     
 temps = []
+#loop FOREVER
 while True:
-    temp = temperature()
+    #gets the temperature from the board, -3 to adjust for the board heating up
+    temp = (temperature() - 3)
+    #adds the temp to the list
     temps.append(temp)
     if button_a.was_pressed():
         ScrollCurrentTemp()
@@ -34,6 +46,11 @@ while True:
         ScrollAvgTemp()
     weather(temp)
     sleep(1000)
+    #prints the average temperature every minute
+    if len(temps) == 60:
+        print(str(getAvg()))
+    #clears the average after 10 minutes
     if len(temps) == 600:
         temps[:]=[]
+        
     
